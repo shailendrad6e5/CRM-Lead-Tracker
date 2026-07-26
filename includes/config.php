1f6c10b36ecdd7a6964789ce4f1c4c8849e683d5
shailendrad6e5 <shailendrad6e5@github.com>
2026-07-26 23:08:38 +0530

    Fix BASE_URL to support lead-tracker.infinityfree.me domain

--- includes/config.php
+++ includes/config.php
@@ -6,7 +6,11 @@ define('SITE_TITLE', 'Lead Management CRM');
 
 
 // Base URL for the application
-define('BASE_URL', 'http://crmleadtracker.free.nf');
+if (isset($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '127.0.0.1')) {
+    define('BASE_URL', '/CRM-Lead-Tracker');
+} else {
+    define('BASE_URL', '');
+}
 
 // Session configuration for security
 ini_set('session.cookie_httponly', 1);
